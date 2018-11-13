3f622b42f7c3e98aa9036b309801ffc1fc04f6f5
Pedro Sanchez <pedsaalv@gmail.com>
2018-11-13 17:20:43 +0000

    Fixed rounding logarithm issues. IMPORTANT: Use Pyhon 3

--- lucky_n.py
+++ lucky_n.py
@@ -1,14 +1,16 @@
 import math
 
 lo = 0
-hi = 10 #ATM, constrained to be a power of ten
+hi = 1000 #ATM, constrained to be a power of ten
 
-t = math.log(hi, 10)
+t = math.ceil(math.log(hi, 10))
+
+print(t)
 
 luckyNumbers = 0
 
 for i in range(int(t)) :
-    #print("Before iteration " + str(i) + " the amount of lucky numbers is " + str(luckyNumbers))
+    print("Before iteration " + str(i) + " the amount of lucky numbers is " + str(luckyNumbers))
     x = 8 * luckyNumbers
     luckyNumbers = x + 2 * (9 ** i)
 
