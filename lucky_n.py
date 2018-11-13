17b9a292b613b55c559d98970fcb9971d928903e
Pedro Sanchez <pedsaalv@gmail.com>
2018-11-13 17:31:43 +0000

    Introduced user input for the upper limit

--- lucky_n.py
+++ lucky_n.py
@@ -1,16 +1,21 @@
 import math
 
+inLimit = input("Write a high search limit (power of 10): ")
+
+while int(inLimit) <= 0:
+    inLimit = input("Not valid input. Please, write a high search limit (power of 10): ")
+
 lo = 0
-hi = 1000 #ATM, constrained to be a power of ten
+hi = int(inLimit) #ATM, constrained to be a power of ten
 
 t = math.ceil(math.log(hi, 10))
 
-print(t)
+#print(t)
 
 luckyNumbers = 0
 
 for i in range(int(t)) :
-    print("Before iteration " + str(i) + " the amount of lucky numbers is " + str(luckyNumbers))
+    #print("Before iteration " + str(i) + " the amount of lucky numbers is " + str(luckyNumbers))
     x = 8 * luckyNumbers
     luckyNumbers = x + 2 * (9 ** i)
 
